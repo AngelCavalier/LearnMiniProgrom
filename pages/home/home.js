bb678a0ec26d1f57dc964090c30b5bbe6a9eedf4
hexowei <hexowei@tencent.com>
2021-09-05 21:15:51 +0800

    feat:初始化目录

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,6 @@
 // pages/home/home.js
+import request from '../../service/network.js'
+
 Page({
 
   /**
@@ -12,7 +14,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 1.请求轮播图以及推荐数据
+    request({
+      url: 'http://152.136.185.210:7878/api/m5/home/multidata',
+    }).then(res=>{
+      console.log(res)
+    })
   },
 
   /**
